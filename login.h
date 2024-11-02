98d450ed20542481a0c02a20a600800f11fcae65
arya892004 <Aryasinghkaushik@gmail.com>
2024-11-02 21:33:16 +0530

    Update login.h

--- login.h
+++ login.h
@@ -5,6 +5,10 @@
 #pragma once
 //#include<iostream>
 #include <string>
+#include <vector>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 class login
@@ -18,3 +22,40 @@ public:
     void Registration();
     void ForgotPassword();
 };
+#pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+enum Status { SUCCESS, FAILURE, NOT_VERIFIED };
+
+class User {
+private:
+    string username;
+    string hashedPassword;
+    string securityQuestion;
+    string email;
+    int isVerified;
+    string verificationCode;
+
+    string hashPassword(const string& password);
+    string generateVerificationCode();
+
+public:
+    User(const string& uname, const string& password, const string& secQuestion, const string& mail);
+    Status verifyPassword(const string& password);
+    string getUsername() const;
+    string getEmail() const;
+    int getVerificationStatus() const;
+    void setVerified();
+    string getSecurityQuestion() const;
+    void sendVerificationEmail();
+    Status verifyEmail(const string& code);
+    void setPassword(const string& newPassword);
+};
+
